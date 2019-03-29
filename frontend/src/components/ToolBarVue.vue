8697801c84f649134b5f66b2026af34a1c222e1f
Bruce Perry <qcepicbruce@hotmail.com>
2019-03-29 15:04:27 -0400

    fixed toolbar responsiveness

--- frontend/src/components/ToolBarVue.vue
+++ frontend/src/components/ToolBarVue.vue
@@ -1,24 +1,29 @@
 <template>
   <nav>
-    <v-toolbar>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>{{ title }}</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <v-btn flat>
-          <v-icon left>question_answer</v-icon>
-          <span>About</span>
-        </v-btn>
-        <v-btn flat>
-          <v-icon left>assignment_ind</v-icon>
-          <span>Sign Up</span>
-        </v-btn>
-        <v-btn flat>
-          <v-icon left>input</v-icon>
-          <span>Log In</span>
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
+    <v-responsive>
+      <v-toolbar>
+        <v-toolbar-side-icon
+          @click.stop="drawer = !drawer"
+          class="hidden-sm-and-up"
+        ></v-toolbar-side-icon>
+        <v-toolbar-title>{{ title }}</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-xs-only">
+          <v-btn flat>
+            <v-icon left>question_answer</v-icon>
+            <span>About</span>
+          </v-btn>
+          <v-btn flat>
+            <v-icon left>assignment_ind</v-icon>
+            <span>Sign Up</span>
+          </v-btn>
+          <v-btn flat>
+            <v-icon left>input</v-icon>
+            <span>Log In</span>
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </v-responsive>
     <v-navigation-drawer v-model="drawer" absolute temporary>
       <v-list class="pt-0" dense>
         <v-divider></v-divider>
