495bd4489c8f52073805901862a6cf3978a6e9c6
Bruce Perry <qcepicbruce@hotmail.com>
2019-03-29 17:50:18 -0400

    started setup for vue-router

---
+++ frontend/src/components/SearchBarVue.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-text-field
+    prepend-icon="location_on"
+    name="searchSpace"
+    placeholder="Search by City, Country or Space..."
+    type="text"
+  ></v-text-field
+></template>
+
+<script>
+export default {
+  name: "SearchBar"
+};
+</script>
+
+<style scoped></style>
