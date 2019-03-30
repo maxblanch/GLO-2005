482b5da4f35698e2b3447491c7bbccc2325c4bda
Bruce Perry <qcepicbruce@hotmail.com>
2019-03-30 18:10:46 -0400

    started
    installed vue-authenticate, vue-axios and axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,9 +5,22 @@ import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css"; // Ensure you are using css-loader
 import App from "./App";
 import router from "./router";
+import VueAxios from "vue-axios";
+import VueAuthenticate from "vue-authenticate";
+import axios from "axios";
 
 Vue.config.productionTip = false;
 Vue.use(Vuetify);
+Vue.use(VueAxios, axios);
+Vue.use(VueAuthenticate, {
+  baseUrl: "", // API domain
+  providers: {
+    github: {
+      clientId: "",
+      redirectUri: "http://localhost:8080/cities" // client app callback
+    }
+  }
+});
 
 /* eslint-disable no-new */
 new Vue({
