7bad0c53679699cf30bb5e02012b77e2aa8a4286
Max Blanchard <blanchard.mx@gmail.com>
2019-04-08 10:47:13 -0400

    Refactored Reviews & Managers for ORM

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,9 @@ from flask_restful import Api
 from flask_jwt import JWT
 from mysql.connector import MySQLConnection, Error
 from resources.coworker import Coworkers, Coworker, CoworkerRegister
+from resources.manager import Managers, Manager, ManagerRegister
 from resources.cwspace import Cwspaces, Cwspace
+from resources.review import Reviews, Review
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:glo2005@localhost:3306/WeShare'
@@ -17,8 +19,15 @@ api.add_resource(Coworkers, '/coworkers')
 api.add_resource(Coworker, '/coworker/<int:id>')
 api.add_resource(CoworkerRegister, '/coworker/register')
 
+api.add_resource(Managers, '/managers')
+api.add_resource(Manager, '/manager/<int:id>')
+api.add_resource(ManagerRegister, '/manager/register')
+
 api.add_resource(Cwspaces, '/cwspaces')
 api.add_resource(Cwspace, '/cwspace/<int:id>')
+
+api.add_resource(Reviews, '/reviews')
+api.add_resource(Review, '/review/<int:cws_id>_<int:coworker_id>')
 
 if __name__ == '__main__':
     from db import db
---
+++ backend/models/Manager.py
@@ -0,0 +1,67 @@
+from db import db
+import hashlib
+
+class ManagerModel(db.Model):
+    __tablename__ = 'Manager'
+
+    manager_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(45), nullable=False)
+    last_name = db.Column(db.String(45), nullable=False)
+    email = db.Column(db.String(45), unique=True, nullable=False)
+    gender = db.Column(db.String(45), nullable=False)
+    username = db.Column(db.String(45), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    address = db.Column(db.String(45), nullable=False)
+    postal_area = db.Column(db.String(45), nullable=False)
+    city = db.Column(db.String(45), nullable=False)
+    state = db.Column(db.String(45), nullable=False)
+    country = db.Column(db.String(45), nullable=False)
+
+    def __init__(self, first_name, 
+                       last_name, 
+                       email, 
+                       gender, 
+                       username, 
+                       password, 
+                       address, 
+                       postal_area, 
+                       city, 
+                       state, 
+                       country):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.gender = gender
+        self.username = username
+        self.password = self.hashPassword(password)
+        self.address = address
+        self.postal_area = postal_area
+        self.city = city
+        self.state = state
+        self.country = country
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def find_by_id(cls, id):
+        return cls.query.filter_by(manager_id=id).first()
+
+    @staticmethod
+    def hashPassword(password):
+        return hashlib.md5(password.encode("utf")).hexdigest()
+    
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+    
+    def json(self):
+        return {
+            'id': self.manager_id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'gender': self.gender,
+            'country': self.country,
+            'Password': self.password
+        }
