0d49a46d56310b17db1cee2d1a031d785b54b9b6
Max Blanchard <blanchard.mx@gmail.com>
2019-04-17 12:48:20 -0400

    Vuejs in a container and docker-compose

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -12,8 +12,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: "localhost", // can be overwritten by process.env.HOST
-    port: 8888, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    host: "0.0.0.0", // can be overwritten by process.env.HOST
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
@@ -35,6 +35,8 @@ module.exports = {
   },
 
   build: {
+    host: "0.0.0.0", // can be overwritten by process.env.HOST
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     // Template for index.html
     index: path.resolve(__dirname, "../dist/index.html"),
 
